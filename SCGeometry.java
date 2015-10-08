02b1901b466e13f706dc87237ac9fa3ba8f44ce8
Patrick Wooden <s960339@kcs29474m.knoxschools.ad>
2015-10-08 16:00:32 -0400

    10/8/15 Changes

--- SCGeometry.java
+++ SCGeometry.java
@@ -4,7 +4,7 @@ public class SCGeometry {
 	public static double radius, length, width, height, density, a, b, circumference, volume, sa, mass, weight;
 	public static int material;
 	public static final double g = 9.8, tons = 907.18474;
-	public static boolean menudecision = false, maindecision = false;
+	public static boolean menudecision = false, maindecision = false, endresponse = false;
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -59,44 +59,46 @@ public class SCGeometry {
 		System.out.println("\t6 - Sphere");
 		System.out.println("\t7 - Spherical Segment");
 		System.out.println("\t8 - Tube");
+		System.out.println("-----------------------------");
+		System.out.println("\t0 - Quit program");
 		int shape = scan.nextInt();
 		while (!menudecision)  {
 			
 			switch(shape) {
+			case 0:// quit
+				menudecision = true;
+				break;
 			case 1:// capsule
-				System.out.print("Input the r then a of the capsule: ");
-				radius = scan.nextDouble();
+				getRadius();
+				System.out.print("Input the a of the capsule: ");
 				a = scan.nextDouble();
 				circumference = (2 * Math.PI * radius);
 				volume = (Math.PI * Math.pow(radius, 2)) * ((4/3)*radius + a);
 				sa = circumference * (2 * radius + a);
-				System.out.println("The circumference is " + fmt.format(circumference));
 				System.out.println("The volume is " + fmt.format(volume));
 				System.out.println("The surface area is " + fmt.format(sa));
 				menudecision = true;
 				break;
 			case 2:// circular cone
-				System.out.println("Input the r then h of the cone: ");
-				radius = scan.nextDouble();
+				getRadius();
+				System.out.println("Input the height of the cone: ");
 				height = scan.nextDouble();
 				double volumepart = (Math.PI * Math.pow(radius, 2) * height);
 				double oneThird = 0.3333333333333;
+				double slant = Math.sqrt(Math.pow(radius, 2) + Math.pow(height, 2));
 				volume = oneThird * volumepart;
-				double slant = Math.sqrt(Math.pow(height, 2) + Math.pow(radius, 2));
 				sa = (Math.PI * Math.pow(radius, 2)) + (Math.PI * radius * slant);
-				System.out.println("The volume is " + fmt.format(volume));
-				System.out.println("The slant height is " + fmt.format(slant));
-				System.out.println("The surface area is " + fmt.format(sa));
+				answers();
 				menudecision = true;
 				break;
 			case 3: // circular cylinder
-				System.out.println("Input the r then h of the cylinder: ");
+				getRadius();
+				System.out.println("Input the height of the cylinder: ");
 				radius = scan.nextDouble();
 				height = scan.nextDouble();
 				volume = Math.PI * Math.pow(radius, 2) * height;
 				sa = (2 * Math.PI * radius * height) + (2 * (Math.PI * Math.pow(radius, 2)));
-				System.out.println("The volume is " + fmt.format(volume));
-				System.out.println("The surface area is " + fmt.format(sa));
+				answers();
 				menudecision = true;
 				break;
 			case 4:// cube
@@ -104,28 +106,21 @@ public class SCGeometry {
 				length = scan.nextDouble();
 				volume = Math.pow(length, 3);
 				sa = 6 * Math.pow(length, 2);
-				System.out.println("The volume is " + fmt.format(volume));
-				System.out.println("The surface area is " + fmt.format(sa));
+				answers();
 				menudecision = true;
 				break;
 			case 5:// hemisphere
-				System.out.print("Input the radius of the hemisphere: ");
-				radius = scan.nextDouble();
+				getRadius();
 				volume = (2.0 / 3.0) * Math.PI * Math.pow(radius, 3);
-				circumference = 2 * Math.PI * radius;
 				sa = 3 * Math.PI * Math.pow(radius, 2);
-				System.out.println("The volume is " + fmt.format(volume));
-				System.out.println("The circumference is " + fmt.format(circumference));
-				System.out.println("The surface area is " + fmt.format(sa));
+				answers();
 				menudecision = true;
 				break;
 			case 6:// sphere
-				System.out.print("Insert the radius of the sphere: ");
-				radius = scan.nextDouble();
+				getRadius();
 				volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
 				sa = 4 * Math.PI * Math.pow(radius, 2);
-				System.out.println("The volume is " + fmt.format(volume));
-				System.out.println("The surface area is " + fmt.format(sa));
+				answers();
 				menudecision = true;
 				break;
 			case 7:// Spherical Segment
@@ -134,6 +129,9 @@ public class SCGeometry {
 				a = scan.nextDouble();
 				b = scan.nextDouble();
 				volume = (1 / 6) * Math.PI * height * ((3 * Math.pow(a, 2)) + (3 * Math.pow(b, 2)) + Math.pow(height, 2));
+				double R = Math.sqrt((Math.pow(a-b, 2) + Math.pow(height, 2)) / 4 * Math.pow(height, 2));
+				sa = 2 * Math.PI * R * height;
+				answers();
 				menudecision = true;
 				break;
 			case 8:// tube
@@ -143,8 +141,11 @@ public class SCGeometry {
 				double v1 = 2 * Math.PI * Math.pow(r1, 2);
 				double v2 = 2 * Math.PI * Math.pow(r2, 2);
 				volume = v1 - v2;
+				double sa1 = 2 * Math.PI * r1 * height;
+				double sa2 = 2 * Math.PI * r2 * height;
+				sa = sa1 + sa2;
 				menudecision = true;
-				System.out.println("The volume is " + fmt.format(volume));
+				answers();
 				break;
 			default:
 				System.out.println("Did you really think I'd fall for that? Try again.");
@@ -153,7 +154,6 @@ public class SCGeometry {
 				
 			}// end of switch-shape
 		}// end of while-loop shape
-		density();
 	}// end of menu
 	
 	public static void density(){
@@ -163,10 +163,40 @@ public class SCGeometry {
 		System.out.println("Or, it's also " + fmt.format(mass / tons) + " tons");
 		weight = mass * g;
 		System.out.println("The weight of the given figure is " + fmt.format(weight) + " Newtons");
-		
-		
-		
+		System.out.print("Insert a 0 to quit, or a 1 to go back to menu: ");
+		loopProgram();
+			
 	}// end of density
+	
+	public static double getRadius() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("What is the radius?: ");
+		radius = scan.nextDouble();
+		return radius;
+	}// end of getRadius
+	
+	public static void answers() {
+		System.out.println("The volume is " + volume);
+		System.out.println("The surface area is " + sa);
+		density();
+	}// end of answers
+	
+	public static void loopProgram() {
+		Scanner scan = new Scanner(System.in);
+		int response = scan.nextInt();
+		while (!endresponse) {
+		switch (response) {
+		case 0:
+			endresponse = true;
+			break;
+		case 1:
+			menu();
+			break;
+		default:
+			System.out.println("Please insert a 0 or 1.");
+		}// end of switch-case response
+		}// end of while-loop end response
+	}//end of loopProgram
 	
 	
 
